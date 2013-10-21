83752c79228de1aa8810d4574e85e188c1d43639
Paulo Cunha <pcunha03@gmail.com>
2013-10-21 05:06:36 -0700

    Homework 1 - Complete

--- hw1/public/lib/attr_accessor_with_history.rb
+++ hw1/public/lib/attr_accessor_with_history.rb
@@ -3,8 +3,33 @@ class Class
     attr_name = attr_name.to_s # make sure it's a string
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
+    
     class_eval %Q{
-      # YOUR CODE HERE
+      def #{attr_name}=(value)
+        if !defined? @#{attr_name}_history
+          @#{attr_name}_history = [nil]
+        else
+          @#{attr_name}_history << @#{attr_name}
+        end
+        @#{attr_name} = value
+      end
     }
   end
 end
+
+# Testing for 'attr_accessor_with_history'
+class Foo 
+  attr_accessor_with_history :bar
+end
+
+f = Foo.new        
+f.bar = 3          # => 3
+f.bar = :wowzo     # => :wowzo
+f.bar = 'boo!'     # => 'boo!'
+f.bar_history      # => [nil, 3, :wowzo]
+
+f = Foo.new
+f.bar = 1 ; f.bar = 2
+g = Foo.new
+g.bar = 3 ; g.bar = 4
+g.bar_history
