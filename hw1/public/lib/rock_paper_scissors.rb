d37930f18d86190cfbd3c34030548ba053d5351e
Paulo Cunha <pcunha03@gmail.com>
2013-10-21 03:50:38 -0700

    Homework 1 - Part 3 - Complete

--- hw1/public/lib/rock_paper_scissors.rb
+++ hw1/public/lib/rock_paper_scissors.rb
@@ -1,14 +1,26 @@
 class RockPaperScissors
-
+    
   # Exceptions this class can raise:
   class NoSuchStrategyError < StandardError ; end
-
+  
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    
+    raise NoSuchStrategyError, "Strategy must be one of R,P,S" unless player1[1] =~ /[RSP]/i and player2[1] =~ /[RSP]/i
+    
+    # refactor
+    return player1 if player1[1] == "R" && player2[1] == "S"
+    return player1 if player1[1] == "S" && player2[1] == "P"
+    return player1 if player1[1] == "P" && player2[1] == "R"
+    return player2 if player1[1] == "S" && player2[1] == "R"
+    return player2 if player1[1] == "P" && player2[1] == "S"
+    return player2 if player1[1] == "R" && player2[1] == "P"
+    player1
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+    return winner(tournament[0],tournament[1]) if tournament[0][0].is_a? String
+    
+    return tournament_winner([tournament_winner(tournament[0]),tournament_winner(tournament[1])])
   end
 
 end
