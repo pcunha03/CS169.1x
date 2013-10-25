a0a64cabbe86d0828acc654b246e5008b7c5db10
Paulo Cunha <pcunha03@gmail.com>
2013-10-25 04:19:06 -0700

    Homework 1.5 - Part 2 - Complete

--- hw1.5/public/lib/oracle_of_bacon.rb
+++ hw1.5/public/lib/oracle_of_bacon.rb
@@ -52,6 +52,7 @@ class OracleOfBacon
       
   class Response
     attr_reader :type, :data
+    
     # create a Response object from a string of XML markup.
     def initialize(xml)
       @doc = Nokogiri::XML(xml)
@@ -59,18 +60,35 @@ class OracleOfBacon
     end
 
     private
-
+    
+    # Homework 1.5 - Part 2
     def parse_response
       if ! @doc.xpath('/error').empty?
         parse_error_response
-      # your code here: 'elsif' clauses to handle other responses
-      # for responses not matching the 3 basic types, the Response
-      # object should have type 'unknown' and data 'unknown response'         
+      elsif ! @doc.xpath('/link').empty?
+        parse_graph_response
+      elsif ! @doc.xpath('/spellcheck').empty?
+        parse_spellcheck_response
+      else
+        parse_unknown_response
       end
     end
+    
     def parse_error_response
       @type = :error
       @data = 'Unauthorized access'
+    end
+    def parse_graph_response
+      @type = :graph
+      @data = @doc.text.split(/\n/).compact.reject{ |x| x.empty? }.each { |x| x.strip! }
+    end
+    def parse_spellcheck_response
+      @type = :spellcheck
+      @data = @doc.text.split(/\n/).compact.reject{ |x| x.empty? }.each { |x| x.strip! }
+    end
+    def parse_unknown_response
+      @type = :unknown
+      @data = 'Unknown response type'
     end
   end
 end
